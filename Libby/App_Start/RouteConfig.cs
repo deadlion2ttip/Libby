e95d28e5bb4cdbf4f9475b669ced87d9dd277587
deadion2ttip <39771562+deadion2ttip@users.noreply.github.com>
2018-10-23 13:35:08 -0400

    Learning about action results and parameters.

--- Libby/App_Start/RouteConfig.cs
+++ Libby/App_Start/RouteConfig.cs
@@ -14,6 +14,9 @@ namespace Libby
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                 ""
+                );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- Libby/Controllers/BooksController.cs
+++ Libby/Controllers/BooksController.cs
@@ -16,5 +16,27 @@ namespace Libby.Controllers
 
             return View(book);
         }
+
+        //GET: Books/Edit/ID
+        public ActionResult Edit(int id)
+        {
+            return Content("id= " + id);
+        }
+
+        // GET: Books
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
     }
 }
